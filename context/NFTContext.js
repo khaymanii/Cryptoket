8013f87ca98fa5d2ee8f9f61f6a34fd696abd021
khaymanii <kennethakpo61@gmail.com>
2024-11-29 20:08:40 +0100

    finished project

--- context/NFTContext.js
+++ context/NFTContext.js
@@ -1,5 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 import Web3Modal from "web3modal";
 import { ether } from "ethers";
 import axios from "axios";
@@ -8,11 +9,15 @@ import { MarketAddress, MarketAddressABI } from "./constants";
 
 const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
 
+const fetchContract = (signerOrProvider) =>
+  new ethers.Contract(MarketAddress, MarketAddressABI, signerOrProvider);
+
 export const NFTContext = React.createContext();
 
 export const NFTProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
   const nftCurrency = "ETH";
+  const router = useRouter();
 
   const checkIfWalletIsConnected = async () => {
     if (!window.ethereum) return alert("Please install Metamask");
@@ -28,6 +33,7 @@ export const NFTProvider = ({ children }) => {
 
   useEffect(() => {
     checkIfWalletIsConnected();
+    createSale();
   }, []);
 
   const connectWallet = async () => {
@@ -52,9 +58,46 @@ export const NFTProvider = ({ children }) => {
     }
   };
 
+  const createNFT = async (formInput, fileUrl, router) => {
+    const { name, description, price } = formInput;
+
+    if (!name || !description || !price || !fileUrl) return;
+
+    const data = JSON.stringify({ name, description, image: fileUrl });
+
+    try {
+      const added = await client.add(data);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+
+      await createSale(url, price);
+
+      router.push("/");
+    } catch (error) {
+      console.log("Error uploading file to IPFS");
+    }
+  };
+
+  const createSale = async (url, formInputPrice) => {
+    const web3Modal = new Web3Modal();
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const price = ethers.utils.parseUnits(formInputPrice);
+    const contract = fetchContract(signer);
+
+    console.log(contract);
+  };
+
   return (
     <NFTContext.Provider
-      value={{ nftCurrency, connectWallet, currentAccount, uploadToIPFS }}
+      value={{
+        nftCurrency,
+        connectWallet,
+        currentAccount,
+        uploadToIPFS,
+        createNFT,
+      }}
     >
       {children}
     </NFTContext.Provider>
